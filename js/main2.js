308dbd7f19d9823f2205e8656d15bdcd42b292cb
karu-10 <79619837+karu-10@users.noreply.github.com>
2021-06-03 02:46:40 +0900

    Add files via upload

--- js/main2.js
+++ js/main2.js
@@ -1,7 +1,7 @@
 'use strict';
 {
+  // メニューバーに関するコード
   const menuItems=document.querySelectorAll('.menu li a');
-
   menuItems.forEach(clickedItem => {
     clickedItem.addEventListener('click',e =>{
       menuItems.forEach(item => {
@@ -11,14 +11,15 @@
     });
   });
 
+  // ロード画面に関するコード
   window.onload = ()=>{
     const loader = document.getElementById('loader');
     loader.classList.add('loaded');
  }
 
+ //ゲームの説明タブに関するコード  
  const desItems=document.querySelectorAll('.description li a');
  const contents=document.querySelectorAll('.content');
-
  desItems.forEach(clickeditem => {
    clickeditem.addEventListener('click',e => {//クリックしたliのaだけを見る
      // a要素のリンク先に遷移する規定の動作をキャンセルするため
@@ -36,4 +37,30 @@
      document.getElementById(clickeditem.dataset.id).classList.add('active');
    });
  });
+
+  // targetとrootの交差に関するAPI
+  // targetは画像、rootはviewport=ウインドウ
+  const targets=document.querySelectorAll('section');
+  
+  function callback(entries,obs){//交差したtargetは引数で受け取れる（配列で）
+    // デフォルトだとtargetがrootに0%交差したときに実行される
+    console.log(entries);
+    //監視開始時は全てのtargetが入っている、交差したときは交差したtargetだけが入る
+    entries.forEach(entry =>{
+      if(!entry.isIntersecting){
+        return;
+      }
+      entry.target.classList.add('appear');
+      obs.unobserve(entry.target);//監視を止める
+    });
+  }
+
+  const options = {//オブジェクト形式
+    threshold: 0.3,
+  };
+  //始まった時も実行される
+  const observer= new IntersectionObserver(callback,options);
+  targets.forEach(target => {
+    observer.observe(target);
+  });
 }
